7c529752c93ae4bea2d23c114eeb58c13b1aac64
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-17 12:23:33 -0600

    Changed not authorized error code to 401

--- auth/requiresAuth-middleware.js
+++ auth/requiresAuth-middleware.js
@@ -4,6 +4,6 @@ module.exports = (req, res, next) => {
     if (req.session && req.session.username) {
         next();
     } else {
-        res.status(400).json({ message: "You are not authorized." })
+        res.status(401).json({ message: "You are not authorized." })
     }
 }
